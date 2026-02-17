ed7734b2bdae9527abb8b8618917e6452bd4054b
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-02-17 21:20:00 +0330

    [implement] navbar

--- src/components/main/main.component.tsx
+++ src/components/main/main.component.tsx
@@ -1,6 +1,7 @@
 import { type ReactNode } from 'react';
-import MapWrapperComponent from '@/components/map/components/map-wrapper.component';
 import { getStations } from '@/services/stations.service';
+import MapWrapperComponent from '@/components/map/components/map-wrapper.component';
+import NavbarComponent from '@/components/navbar/navbar.component';
 
 export default async function MainComponent(): Promise<ReactNode> {
 	const response = await getStations();
@@ -8,6 +9,7 @@ export default async function MainComponent(): Promise<ReactNode> {
 	return (
 		<div>
 			<MapWrapperComponent stations={response} />
+			<NavbarComponent />
 		</div>
 	);
 }
